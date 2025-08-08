2d1a6c1c6a703d765d69015085f427be42a58b40
Bob Rundle <rundle@rundle.com>
2025-08-08 08:43:31 -0700

    Working example

--- app/api/counter/route.ts
+++ app/api/counter/route.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 interface Context {
-  params: undefined;
+  params: Promise<any>;
 }
 
 export async function POST(request: NextRequest, context: Context) {
